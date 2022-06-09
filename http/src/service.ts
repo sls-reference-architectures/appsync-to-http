f7bedf31df429233fbd800c772774a27a0bd791b
Seth Orell <seth.orell@gmail.com>
2022-06-08 22:27:43 -0500

    pre-commit?

--- http/src/service.ts
+++ http/src/service.ts
@@ -5,12 +5,16 @@ import { Product } from './models';
 export const getProduct = async (input: GetProductInput): Promise<Product> => {
   Logger.debug('In service.getProduct()', { input });
   const defaultProduct: Product = {
-    id: 'x',
+    id: 'xyz',
     name: 'placeholder',
     price: 0.42,
   };
-  
+
   return defaultProduct;
+};
+
+export const placeholder = () => {
+  Logger.debug('placeholder export');
 };
 
 interface GetProductInput {
