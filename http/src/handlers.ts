07f268b618116c82fb7e99bd98b4da580256cf98
Seth Orell <seth.orell@gmail.com>
2022-06-08 22:33:09 -0500

    Format

--- http/src/handlers.ts
+++ http/src/handlers.ts
@@ -1,17 +1,23 @@
 import Logger from '@dazn/lambda-powertools-logger';
 // import { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
-import { APIGatewayProxyEventMiddyNormalized, Product } from './models';
 
+import { APIGatewayProxyEventMiddyNormalized, Product } from './models';
 import * as service from './service';
 
 // type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>
 
-export const getProduct = async (event: APIGatewayProxyEventMiddyNormalized<null>): Promise<Product> => {
+export const getProduct = async (
+  event: APIGatewayProxyEventMiddyNormalized<null>,
+): Promise<Product> => {
   Logger.debug('In getProduct()', { event });
-  const { pathParameters: { storeId = '', productId = '' } } = event;
+  const {
+    pathParameters: { storeId = '', productId = '' },
+  } = event;
   const product = await service.getProduct({ storeId, productId });
 
   return product;
 };
 
-export const placeHolder = () => { };
+export const placeHolder = () => {
+  Logger.debug('placeholder');
+};
