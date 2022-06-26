a29306820e057ac66d56d9a7c4c6a4bc8f2b8394
Seth Orell <seth.orell@gmail.com>
2022-06-26 16:48:59 -0500

    Add limit param to vtl

--- appSync/test/getProducts.e2e.test.ts
+++ appSync/test/getProducts.e2e.test.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import retry from 'async-retry';
 
+import { ulid } from 'ulid';
 import { GetProductsQuery, TestHelpers } from '../../common/testHelpers';
 
 const BaseUri = process.env.GRAPH_API_URL ?? '';
@@ -39,5 +40,40 @@ describe('When querying for Products', () => {
       },
       { retries: 3 },
     );
+  });
+
+  describe('using limit', () => {
+    it('should restrict result to limit', async () => {
+      // ARRANGE
+      const storeId = ulid();
+      const { productId: id1 } = await testHelpers.createRandomProductInDb({ storeId });
+      await testHelpers.createRandomProductInDb({ storeId });
+      const requestOptions: AxiosRequestConfig = {
+        headers: {
+          'x-api-key': process.env.GRAPH_API_KEY ?? '',
+          'Content-Type': 'application/json',
+        },
+        validateStatus: () => true,
+      };
+      const input = { storeId, limit: 1 };
+
+      await retry(
+        async () => {
+          // ACT
+          const { data, status } = await axios.post(
+            BaseUri,
+            { query: GetProductsQuery, variables: { input } },
+            requestOptions,
+          );
+
+          // ASSERT
+          expect(status).toEqual(200);
+          expect(data.errors).toBeUndefined();
+          expect(data.data.getProducts.items).toHaveLength(1);
+          expect(data.data.getProducts.items[0].productId).toEqual(id1);
+        },
+        { retries: 3 },
+      );
+    });
   });
 });
