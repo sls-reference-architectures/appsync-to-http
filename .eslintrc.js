564064842b7735f721e22a8b46500ddd2b5a3f25
Seth Orell <seth.orell@gmail.com>
2023-06-07 08:22:41 -0500

    Tidy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,3 @@
-// module.exports = {
-//   env: {
-//     jest: true,
-//     node: true,
-//   },
-//   extends: [
-//     'eslint:recommended',
-//     'plugin:@typescript-eslint/eslint-recommended',
-//     'plugin:@typescript-eslint/recommended',
-//     'airbnb-base',
-//   ],
-//   parser: '@typescript-eslint/parser',
-//   plugins: ['@typescript-eslint'],
-//   root: true,
-//   rules: {
-//     'no-use-before-define': 0,
-//     'import/extensions': 0,
-//     'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.ts'] }],
-//     '@typescript-eslint/no-var-requires': 0,
-//   },
-//   settings: {
-//     'import/resolver': {
-//       typescript: {
-//         alwaysTryTypes: true,
-//       },
-//     },
-//   },
-// };
-
 module.exports = {
   env: {
     jest: true,
